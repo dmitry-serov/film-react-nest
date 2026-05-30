c86afe749760a163c07496ae2a5ab91303d58653
dmitry-serov <frickmen@yandex.ru>
2026-05-30 21:21:22 +0300

    feat: add custom loggers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { AppConfig } from './app.config.provider';
+import { TskvLogger } from './logger/tskv.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const config = app.get<AppConfig>('CONFIG');
 
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
+  app.useLogger(new TskvLogger());
   await app.listen(config.port);
 }
 bootstrap();
